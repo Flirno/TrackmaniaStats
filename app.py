800c7e051a6ea77e1a92c6710b14392d26f76742
Flirno <flirno.gaming@laposte.net>
2021-02-11 12:57:28 +0100

    added cors

--- app.py
+++ app.py
@@ -10,9 +10,12 @@ from flask import Flask, render_template
 import json 
 from datetime import date  
 from datetime import datetime
+from flask_cors import CORS
+
 
 app = Flask(__name__)
 
+CORS(app)
 
 today = str(date.today())
 now = datetime.now()
